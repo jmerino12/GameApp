e88bcba9f5e55d8c26997a63167d238dcdf3f670
Jonathan Meriño B <jonathan.merino@ceiba.com.co>
2023-07-09 18:23:53 -0500

    Filtro por edito y por genero

--- infraestructura/src/main/java/com/example/infraestructura/juego/repositorios/RepositorioDeJuegosRetrofit.kt
+++ infraestructura/src/main/java/com/example/infraestructura/juego/repositorios/RepositorioDeJuegosRetrofit.kt
@@ -13,7 +13,7 @@ import javax.inject.Inject
 class RepositorioDeJuegosRetrofit @Inject constructor(private val servicioDeJuego: ServicioDeJuego) :
     RepositorioRemotoDeJuegos {
 
-    override suspend fun obtenerJuegos(): Flow<List<Juego>> {
+    override fun obtenerJuegos(): Flow<List<Juego>> {
         return flow { emit(servicioDeJuego.obtenerJuegos()) }.map { resultado ->
             resultado.map { juegoDto ->
                 TraductorDeJuegos.desdeDtoHaciaModelo(
--- presentacion/src/main/java/com/example/gameapp/ui/pantallas/inicio/PantallaDeInicio.kt
+++ presentacion/src/main/java/com/example/gameapp/ui/pantallas/inicio/PantallaDeInicio.kt
@@ -1,16 +1,24 @@
 package com.example.gameapp.ui.pantallas.inicio
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FilterChip
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import com.example.dominio.juego.modelo.Juego
 import com.example.gameapp.ui.compatido.CargandoContenido
@@ -20,6 +28,10 @@ import com.example.gameapp.ui.compatido.CargandoContenido
 fun PantallaDeInicio(
     modifier: Modifier = Modifier,
     estadoDeUi: PantallaDeInicioEstadosDeUi,
+    valorDelFiltro: String,
+    filtroDeGenero: (String) -> Unit,
+    filtroDeEditor: (String) -> Unit,
+    resetearFiltro: () -> Unit,
     juegoClick: (Int) -> Unit,
 ) {
     Scaffold { paddingValues ->
@@ -28,10 +40,17 @@ fun PantallaDeInicio(
             estadoDeUi.cargando,
             estadoDeUi.juegoDeApi,
             estadoDeUi.juegosFavoritos,
+            estadoDeUi.generos,
+            estadoDeUi.editores,
+            valorDelFiltro,
+            filtroDeGenero,
+            filtroDeEditor,
+            resetearFiltro,
             juegoClick,
         )
     }
 }
+
 
 @Composable
 fun ContenidoDePantallaDeInicio(
@@ -39,12 +58,22 @@ fun ContenidoDePantallaDeInicio(
     cargando: Boolean,
     juegosDeApi: List<Juego>,
     juegosFavoritos: List<Juego>,
+    generosDeJuego: List<String>,
+    editores: List<String>,
+    valorDelFiltro: String,
+    filtroDeGenero: (String) -> Unit,
+    filtroDeEditor: (String) -> Unit,
+    resetearFiltro: () -> Unit,
+
     juegoClick: (Int) -> Unit,
 ) {
     CargandoContenido(
         cargando = cargando
     ) {
         Column {
+            FiltroPor("Generos", generosDeJuego, valorDelFiltro, filtroDeGenero, resetearFiltro)
+            FiltroPor("Editores", editores, valorDelFiltro, filtroDeEditor, resetearFiltro)
+
             if (juegosDeApi.isNotEmpty()) {
                 ListaDeJuegos(
                     juegoClick = juegoClick,
@@ -59,9 +88,44 @@ fun ContenidoDePantallaDeInicio(
                     juegos = juegosFavoritos,
                     tipoDeContenido = "Juegos Favoritos"
                 )
+            } else if (juegosDeApi.isEmpty() && juegosFavoritos.isEmpty()) {
+                Text(text = "No tenemos nada para mostrar")
             }
+
         }
 
+    }
+}
+
+@Composable
+@OptIn(ExperimentalMaterial3Api::class)
+private fun FiltroPor(
+    tipoDeFiltro: String,
+    filtros: List<String>,
+    valorDelFiltro: String,
+    seleccionarFiltro: (String) -> Unit,
+    resetearFiltro: () -> Unit,
+
+    ) {
+    LazyRow(
+        horizontalArrangement = Arrangement.spacedBy(4.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        item {
+            Text(text = tipoDeFiltro)
+        }
+        items(filtros) {
+            var estaSeleccionado = valorDelFiltro == it
+            val leadingIcon: @Composable () -> Unit = { Icon(Icons.Default.Check, null) }
+            FilterChip(
+                selected = estaSeleccionado,
+                onClick = {
+                    if (it == "Todos") resetearFiltro() else seleccionarFiltro(it)
+                },
+                label = { Text(it) },
+                leadingIcon = if (estaSeleccionado) leadingIcon else null
+            )
+        }
     }
 }
 
